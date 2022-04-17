5cbcad9b9c2796cc7aaa6699feefe377cfa93358
leetomlee123 <leetomlee123@gmail.com>
2022-04-17 18:58:11 +0800

    init

--- lib/pages/listen/listen_view.dart
+++ lib/pages/listen/listen_view.dart
@@ -13,52 +13,51 @@ class ListenPage extends GetView<ListenController> {
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //     // backgroundColor: controller.bgColor.value,
-    //     appBar: _buildAppBar(),
-    //     body: _buildPlayUi());
+    return Scaffold(
+        // backgroundColor: controller.bgColor.value,
+        appBar: _buildAppBar(),
+        body: _buildPlayUi());
 
-    return  Material(
-          shadowColor: Colors.white10,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
+    return Material(
+      shadowColor: Colors.white10,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
               children: [
+                Icon(Icons.menu),
                 const SizedBox(
-                  height: 30,
+                  width: 10,
                 ),
-                Row(
-                  children: [
-                    Icon(Icons.menu),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Obx(() => Offstage(
-                          offstage: !controller.getLink.value,
-                          child: const Text(
-                            '获取资源中...',
-                            style: TextStyle(fontSize: 13),
-                          ),
-                        )),
-                    Spacer(),
-                    IconButton(
-                        onPressed: () {
-                          Get.toNamed(AppRoutes.search);
-                        },
-                        icon: Icon(Icons.search_outlined))
-                  ],
-                ),
-                _buildPlayUi()
+                Obx(() => Offstage(
+                      offstage: !controller.getLink.value,
+                      child: const Text(
+                        '获取资源中...',
+                        style: TextStyle(fontSize: 13),
+                      ),
+                    )),
+                Spacer(),
+                IconButton(
+                    onPressed: () {
+                      Get.toNamed(AppRoutes.search);
+                    },
+                    icon: Icon(Icons.search_outlined))
               ],
             ),
-          ),
-        );
+            _buildPlayUi()
+          ],
+        ),
+      ),
+    );
   }
 
   _buildAppBar() {
     return AppBar(
       leading: Icon(Icons.menu),
-      backgroundColor: Colors.transparent,
       elevation: 0,
       title: Obx(() => Offstage(
             offstage: !controller.getLink.value,
