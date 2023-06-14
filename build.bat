958f81a8418e8b0e7d19f3b3c76deb44ba2ba119
Alex <leetomlee123@gmail.com>
2023-06-14 08:48:28 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 call flutter clean
-call flutter build apk --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm64 --split-per-abi --build-name=6.0.0 --build-number=3 
+call flutter build apk --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm64 --split-per-abi --build-name=6.0.1 --build-number=3 
 
 
 
